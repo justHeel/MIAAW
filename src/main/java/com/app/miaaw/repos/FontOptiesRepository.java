d7070f344c057ed2a79bcd24629494e4e84a3d6e
GiulioRondon <giulio.rondon@student.hu.nl>
2019-04-26 12:21:30 +0200

    idk

--- src/main/java/com/app/miaaw/repos/FontOptiesRepository.java
+++ src/main/java/com/app/miaaw/repos/FontOptiesRepository.java
@@ -1,6 +1,7 @@
 package com.app.miaaw.repos;
 
 import com.app.miaaw.Domain.FontOpties;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,4 @@ import org.springframework.stereotype.Repository;
 */
 @Repository
 public interface FontOptiesRepository extends JpaRepository<FontOpties, Long>, JpaSpecificationExecutor<FontOpties> {
-
 }
