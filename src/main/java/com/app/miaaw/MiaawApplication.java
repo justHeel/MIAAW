e6bdbb9ce1bb5aa26f6f0009f8089f40c3ee8859
Süleyman <suleyman.altindag@student.hu.nl>
2019-05-07 13:38:53 +0200

    imagedescriber update

--- src/main/java/com/app/miaaw/MiaawApplication.java
+++ src/main/java/com/app/miaaw/MiaawApplication.java
@@ -1,8 +1,20 @@
 package com.app.miaaw;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.app.miaaw.WebpageEnhancer.ImageDescriber;
+import com.app.miaaw.WebpageEnhancer.ImageDescriberFinal;
+import com.app.miaaw.WebpageEnhancer.SoupChef;
 import com.cmeza.sdgenerator.annotation.SDGenerator;
 @SDGenerator(
         entityPackage = "com.app.miaaw.Domain",
@@ -17,7 +29,39 @@ import com.cmeza.sdgenerator.annotation.SDGenerator;
 @SpringBootApplication
 public class MiaawApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MiaawApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		//SpringApplication.run(MiaawApplication.class, args);
+		//System.out.println(ImageDescriber.getDescription());
+		
+		//System.out.println(ImageDescriber.getAllDescription());
+		//List<String> urls = ImageDescriber.getAllDescription();
+		//System.out.println(ImageDescriber.getAllDescription());
+		//System.out.println(ImageDescriberFinal.getDescription());
+		
+		List<String> urlOfImages = ImageDescriber.getAllDescription();
+		Map<String, String> descriptions = new HashMap<>();
+		
+		for(String url : urlOfImages) {
+			String description = ImageDescriber.getDescription(url);
+			descriptions.put(url, description);
+		}
+		
+		descriptions.entrySet().stream().forEach(entry -> {
+			System.out.println(">>>>>> Nieuwe plaatje");
+			System.out.println("url: " + entry.getKey());
+			System.out.println("image details: " + entry.getValue());
+		});
+		
+		
+		/*Document link = SoupChef.makeSoup("https://www.amazon.de/gp/switch-language/homepage.html?ie=UTF8&tag=googdeaen-21&ref=pd_sl_17puwuo972_e&hvadid=163602155469&hvpos=1t1&hvexid=&hvnetw=g&hvrand=15699322301001308934&hvpone=&hvptwo=&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9065291&hvtargid=kwd-10573980&language=nl_NL");
+		String s;
+		Elements images = link.select("img");		
+		List<String> list = new ArrayList<String>();
+		
+		for (Element img : images) {
+			s = img.attr("abs:src");
+			list.add(s);
+			System.out.println(list);
+		}*/	
 	}
 }
--- src/main/java/com/app/miaaw/WebpageEnhancer/SoupChef.java
+++ src/main/java/com/app/miaaw/WebpageEnhancer/SoupChef.java
@@ -1,13 +1,19 @@
 package com.app.miaaw.WebpageEnhancer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class SoupChef {
 	public static Document makeSoup(String link) throws IOException {
         String url = link;
+
         Document document = Jsoup.connect(url).get();
         return document;
 	}
